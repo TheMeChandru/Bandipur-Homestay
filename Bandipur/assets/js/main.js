ff8b4b344d53c6a8b7586ab0448e41cd2743fd61
Chandrashekar <chandrashekar93802003@gamil.com>
2024-04-13 14:27:14 +0530

    Done

---
+++ Bandipur/assets/js/main.js
@@ -0,0 +1,50 @@
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+
+
+// Review Count Section
+
+new PureCounter();
+
+
+// Our Services Courosel javascript 
+
+new Swiper('.testimonials-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
+
+
